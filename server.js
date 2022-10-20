bda1fbed7404c2cd152b2e127c61be95bc03ce95
li1115 <598435866@qq.com>
2022-10-20 15:16:55 -0400

    111

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ other source
 *
 * Name: Zijun Li    Student ID: 170055214   Date: 10/06/2022
 *
-* Your app’s URL (from Cyclic) : https://vast-jade-goshawk-veil.cyclic.app
+* Your app’s URL (from Cyclic) : App URL: https://vast-jade-goshawk-veil.cyclic.app
 *
 *************************************************************************/ 
 var dataprep = require(__dirname + "/data_prep.js");
@@ -17,6 +17,7 @@ var express = require("express");
 var app = express();
 
 var path = require("path"); 
+const { allStudents } = require("./data_prep");
 
 var HTTP_PORT = process.env.PORT || 8080; 
 
@@ -38,6 +39,31 @@ app.get("/CPA", (req, res) => {
 
 app.get("/highGPA", (req, res) => {
     dataprep.highGPA().then((data) => {
+        res.json({data});
+    }).catch((err) => {
+        res.json({message: err});
+    })
+});
+
+app.get("/allStudents", (req, res) => {
+    dataprep.allStudents().then((data) => {
+        res.json({data});
+    }).catch((err) => {
+        res.json({message: err});
+    })
+});
+
+app.get('/addStudent', (req, res) => {
+    res.sendFile(path.join(__dirname + "/test3_views/addStudent.html"));
+});
+
+app.post('/addStudent', (req, res) => {
+    const {fullName, studentId, program}  = req.body;
+    res.send({fullName, studentId,program});
+});
+
+app.get("/getStudent", (req, res) => {
+    dataprep.getStudent().then((data) => {
         res.json({data});
     }).catch((err) => {
         res.json({message: err});
